8b6e0f0c95416cc213c8d01bcb1d7d641f539058
Avinash Swaminathan <s.avinash.it.17@nsit.net.in>
2019-08-22 05:25:32 +0530

    Add files via upload

--- hierarchal_Discriminator_f1score.py
+++ hierarchal_Discriminator_f1score.py
@@ -47,10 +47,10 @@ from hierarchal_Discriminator import Discriminator
 from torch.nn import functional as F
 from sklearn.metrics import f1_score,confusion_matrix
 #####################################################################################################
-opt = argparse.Namespace(attn_mode='concat', baseline='self', batch_size=32, batch_workers=4, bidirectional=True, bridge='copy', checkpoint_interval=4000, copy_attention=True, copy_input_feeding=False, coverage_attn=False, coverage_loss=False, custom_data_filename_suffix=False, custom_vocab_filename_suffix=False, data='data/kp20k_tg_sorted/', data_filename_suffix='', dec_layers=1, decay_method='', decoder_size=300, decoder_type='rnn', delimiter_type=0, delimiter_word='<sep>', device=device(type='cuda', index=2
+opt = argparse.Namespace(attn_mode='concat', baseline='self', batch_size=32, batch_workers=4, bidirectional=True, bridge='copy', checkpoint_interval=4000, copy_attention=True, copy_input_feeding=False, coverage_attn=False, coverage_loss=False, custom_data_filename_suffix=False, custom_vocab_filename_suffix=False, data='data/kp20k_sorted/', data_filename_suffix='', dec_layers=1, decay_method='', decoder_size=300, decoder_type='rnn', delimiter_type=0, delimiter_word='<sep>', device=device(type='cuda', index=2
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ), disable_early_stop_rl=False, dropout=0.1, dynamic_dict=True, early_stop_tolerance=4, enc_layers=1, encoder_size=150, encoder_type='rnn', epochs=20, exp='kp20k.rl.one2many.cat.copy.bi-directional', exp_path='exp/kp20k.rl.one2many.cat.copy.bi-directional.20190701-192604', final_perturb_std=0, fix_word_vecs_dec=False, fix_word_vecs_enc=False, goal_vector_mode=0, goal_vector_size=16, gpuid=3, init_perturb_std=0, input_feeding=False, lambda_coverage=1, lambda_orthogonal=0.03, lambda_target_encoder=0.03, learning_rate=0.001, learning_rate_decay=0.5, learning_rate_decay_rl=False, learning_rate_rl=5e-05, loss_normalization='tokens', manager_mode=1, match_type='exact', max_grad_norm=1, max_length=60, max_sample_length=6, max_unk_words=1000, mc_rollouts=False, model_path='model/kp20k.rl.one2many.cat.copy.bi-directional.20190701-192604', must_teacher_forcing=False, num_predictions=1, num_rollouts=3, one2many=True, one2many_mode=1, optim='adam', orthogonal_loss=False, param_init=0.1, perturb_baseline=False, perturb_decay_factor=0.0001, perturb_decay_mode=1, pre_word_vecs_dec=None, pre_word_vecs_enc=None, pretrained_model='model/kp20k.ml.one2many.cat.copy.bi-directional.20190628-114655/kp20k.ml.one2many.cat.copy.bi-directional.epoch=2.batch=54573.total_batch=116000.model', regularization_factor=0.0, regularization_type=0, remove_src_eos=False, replace_unk=True, report_every=10, review_attn=False, reward_shaping=False, reward_type=7, save_model='model', scheduled_sampling=False, scheduled_sampling_batches=10000, seed=9527, separate_present_absent=True, share_embeddings=True, source_representation_queue_size=128, source_representation_sample_size=32, start_checkpoint_at=2, start_decay_at=8, start_epoch=1, target_encoder_size=64, teacher_forcing_ratio=0, timemark='20190701-192604', title_guided=False, topk='G', train_from='', train_ml=False, train_rl=True, truncated_decoder=0, use_target_encoder=False, vocab='data/kp20k_separated', vocab_filename_suffix='', vocab_size=100002, warmup_steps=4000, word_vec_size=100, words_min_frequency=0)
-devices = "cuda:2"
+devices = "cpu"
             
 ##### TUNE HYPERPARAMETERS ##############
 hidden_dim = 150
@@ -68,10 +68,10 @@ def train_one_batch(D_model,one2many_batch, generator, opt,perturb_std):
     else:
         num_predictions = 1
     
-
-    src = src.to(opt.device)
-    src_mask = src_mask.to(opt.device)
-    src_oov = src_oov.to(opt.device)
+    if torch.cuda.is_available():
+        src = src.to(opt.device)
+        src_mask = src_mask.to(opt.device)
+        src_oov = src_oov.to(opt.device)
 #    src = src.cuda()
 #    src_mask = src_mask.cuda()
 #    src_oov = src_oov.cuda()
@@ -196,7 +196,7 @@ def main():
     model = model.to(devices)
     
    # model.load_state_dict(torch.load("model/kp20k.ml.one2many.cat.copy.bi-directional.20190628-114655/kp20k.ml.one2many.cat.copy.bi-directional.epoch=2.batch=54573.total_batch=116000.model"))
-    model.load_state_dict(torch.load("model/kp20k.ml.one2many.cat.copy.bi-directional.20190715-132016/kp20k.ml.one2many.cat.copy.bi-directional.epoch=3.batch=26098.total_batch=108000.model"))
+    model.load_state_dict(torch.load("model/kp20k.ml.one2many.cat.copy.bi-directional.20190715-132016/kp20k.ml.one2many.cat.copy.bi-directional.epoch=3.batch=26098.total_batch=108000.model",map_location=devices))
     generator = SequenceGenerator(model,
                                   bos_idx=opt.word2idx[pykp.io.BOS_WORD],
                                   eos_idx=opt.word2idx[pykp.io.EOS_WORD],
@@ -216,7 +216,8 @@ def main():
     perturb_decay_mode = opt.perturb_decay_mode
     
     D_model = Discriminator(opt.vocab_size,embedding_dim,hidden_dim,n_layers,opt.word2idx[pykp.io.PAD_WORD])
-    D_model.load_state_dict(torch.load("Discriminator_checkpts/hierarchal_attention_Dis1_0.pth.tar"))
+    ## COPY YOUR CHECKPOINT FILE ####
+    ## D_model.load_state_dict(torch.load("Discriminator_checkpts/hierarchal_attention_Dis1_0.pth.tar"))
   
     print("The Discriminator statistics are ",D_model)
     
